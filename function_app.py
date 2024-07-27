9327b687a872b7c7ef4fcab0c5b777d55ecb00fc
Henrico Aldy Ferdian <henricoaldyferdian@hotmail.com>
2024-07-27 18:57:47 +0700

    chore: remove http trigger example

--- function_app.py
+++ function_app.py
@@ -7,27 +7,6 @@ import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 pushy_api_key = os.environ.get('PUSHY_API_KEY')
-
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
-        )
         
 @app.route(route="disaster_demo_trigger")
 def listen_disaster_trigger(req: func.HttpRequest) -> func.HttpResponse:
