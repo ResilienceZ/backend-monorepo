aa118ced305fd6e7d494f87b49eb55332590376c
Henrico Aldy Ferdian <henricoaldyferdian@hotmail.com>
2024-07-28 02:14:49 +0700

    feat: add db model

---
+++ data/disaster_record.py
@@ -0,0 +1,15 @@
+class DisasterRecord:
+    def __init__(self, id, description, severity, scale, longitude, latitude, type, timestamp):
+        self.id = id
+        self.description = description
+        self.severity = severity
+        self.scale = scale
+        self.longitude = longitude
+        self.latitude = latitude
+        self.type = type
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return (f"DisasterRecord(id={self.id}, description={self.description}, severity={self.severity}, "
+                f"scale={self.scale}, longitude={self.longitude}, latitude={self.latitude}, "
+                f"type={self.type}, timestamp={self.timestamp})")
---
+++ data/disaster_report.py
@@ -0,0 +1,13 @@
+class DisasterReport:
+    def __init__(self, id, reporter_uniqueid, longitude, latitude, geohash, type, timestamp):
+        self.id = id
+        self.reporter_uniqueid = reporter_uniqueid
+        self.longitude = longitude
+        self.latitude = latitude
+        self.geohash = geohash
+        self.type = type
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return (f"DisasterReport(id={self.id}, reporter_uniqueid={self.reporter_uniqueid}, longitude={self.longitude}, "
+                f"latitude={self.latitude}, geohash={self.geohash}, type={self.type}, timestamp={self.timestamp})")
